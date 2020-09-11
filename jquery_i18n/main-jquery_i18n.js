aee3d2094e9ed3b0b6750f26e90fe709a6818af8
Anton <anvasilev52@gmail.com>
2020-09-12 00:25:08 +0300

    e

---
+++ jquery_i18n/main-jquery_i18n.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+  let  update_texts = function() { $('body').i18n() };
+  $.i18n().load({
+	'ru': 'i18n/ru.json',
+	'en': 'i18n/en.json'
+  });
+  update_texts();
+  
+  $('.lang-switch').click(function(e) {
+  e.preventDefault();
+  $.i18n().locale = $(this).data('locale');
+  update_texts();
+});
+  
+  
+});
