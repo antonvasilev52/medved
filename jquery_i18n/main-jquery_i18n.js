21c272e54dd760f441d8b5208d40d689e803c917
Anton <anvasilev52@gmail.com>
2020-09-12 00:40:05 +0300

    add fr

--- jquery_i18n/main-jquery_i18n.js
+++ jquery_i18n/main-jquery_i18n.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function() {
   let  update_texts = function() { $('body').i18n() };
   $.i18n().load({
 	'ru': 'i18n/ru.json',
-	'en': 'i18n/en.json'
+	'en': 'i18n/en.json',
+	'fr': 'i18n/fr.json'
   });
   update_texts();
   
