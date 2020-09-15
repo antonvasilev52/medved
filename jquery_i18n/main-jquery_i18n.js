c8379629b6612d15b3f166e8e8e0b61ba2c626e6
Anton <anvasilev52@gmail.com>
2020-09-15 20:36:24 +0300

    Partly JSON

--- jquery_i18n/main-jquery_i18n.js
+++ jquery_i18n/main-jquery_i18n.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function() {
+$(function() {
 //  $("#bear").attr("src","bear_ru.jpg");
   let date = new Date();
-  const japOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',};
+  const japOptions = { year: 'numeric', month: 'short', day: 'numeric' };
   let  update_texts = function() { $('body').i18n() };
   $.i18n().load({
   'en': {
@@ -9,6 +9,7 @@ jQuery(document).ready(function() {
 	'clearlist': ' Clear the list',
 	'heading': 'To-do list for ' + date.toLocaleDateString('en-US'),
 	'name': 'Enter a task',
+	'tooltip': 'Task',
 	'add': 'Add',
 	'help': 'Help'
   },
@@ -17,14 +18,17 @@ jQuery(document).ready(function() {
 	'clearlist': ' Очистить список',
 	'heading': 'Список дел на '  + date.toLocaleDateString('ru-RU'),
 	'name': 'Введите дело',
+	'tooltip': 'Или дельце',
 	'add': 'Добавить',
 	'help': 'Справка'
 	
   },
-  'fr' : {
+  	fr: 'i18n/fr.json',
+	ja: 'i18n/ja.json'
+/*  'fr' : {
     'welcome': 'Bienvenue',
 	'clearlist': ' Effacer la liste',
-	'heading': 'Liste des tâches à faire le '  + date.toLocaleDateString('fr-FR'),
+	'heading': 'Liste des tâches à faire le '  + date.toLocaleDateString('fr-FR',japOptions),
 	'name': 'Saisissez une tache',
 	'add': 'Ajouter',
 	'help': 'Aide'
@@ -36,15 +40,14 @@ jQuery(document).ready(function() {
 	'name': 'やることを入力してください',
 	'add': '追加',
 	'help': 'Aide'
-  }
+  } */
   });
   update_texts();
   
   $('.lang-switch').click(function(e) {
   e.preventDefault();
   $.i18n().locale = $(this).data('locale');
-  let country = $(this).data('locale');
-  $("#bear").attr("src","img/bear_"+country+".jpg");
+  $("#bear").attr("src","img/bear_"+$(this).data('locale')+".jpg");
   update_texts();
 });
   
