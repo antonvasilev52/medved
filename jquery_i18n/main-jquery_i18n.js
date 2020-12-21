3ceac65f7d2a39afb03cb7abb43597aa7723ea26
Anton <anvasilev52@gmail.com>
2020-12-21 17:42:58 +0300

    update 21.12.2020

--- jquery_i18n/main-jquery_i18n.js
+++ jquery_i18n/main-jquery_i18n.js
@@ -1,20 +1,29 @@
 $(function() {
 //  $("#bear").attr("src","bear_ru.jpg");
+
   let date = new Date();
   const japOptions = { year: 'numeric', month: 'short', day: 'numeric' };
-  let  update_texts = function() { $('body').i18n() };
+  let  update_texts = function() { $('body').i18n() 
+    $( '#inputbox' ).prop( {
+    placeholder: $.i18n( 'name' ),
+    title: $.i18n( 'tooltip' ),
+//  value: $.i18n( 'task-value' )
+	});
+  };
   $.i18n().load({
-	en: 'i18n/en.json',
+/*	en: 'i18n/en.json',
 	ru: 'i18n/en.json',
   	fr: 'i18n/fr.json',
 	ja: 'i18n/ja.json'
-/* 'en': {
+*/ 'en': {
     'welcome': 'Welcome',
 	'clearlist': ' Clear the list',
 	'heading': 'To-do list for ' + date.toLocaleDateString('en-US'),
 	'name': 'Enter a task',
 	'tooltip': 'Task',
 	'add': 'Add',
+	'task-value': "Составить список дел",
+	'interface-language': 'Interface language',
 	'help': 'Help'
   },
   'ru' : {
@@ -24,6 +33,8 @@ $(function() {
 	'name': 'Введите дело',
 	'tooltip': 'Или дельце',
 	'add': 'Добавить',
+	'task-value': "Составить список дел",
+	'interface-language': 'Язык интерфейса',
 	'help': 'Справка'
 	
   },
@@ -32,17 +43,21 @@ $(function() {
 	'clearlist': ' Effacer la liste',
 	'heading': 'Liste des tâches à faire le '  + date.toLocaleDateString('fr-FR',japOptions),
 	'name': 'Saisissez une tache',
+	'tooltip': 'Une tache',
 	'add': 'Ajouter',
+	'interface-language': "Langue de l'interface",
 	'help': 'Aide'
   },
   'ja' : {
     'welcome': 'ようこそ',
 	'clearlist': ' 削除',
 	'heading': date.toLocaleDateString('ja-JP',japOptions) + 'のやることリスト ',
-	'name': 'やることを入力してください',
+	'name': 'やることを入力して',
+	'tooltip': 'やること',
 	'add': '追加',
-	'help': 'Aide'
-  } */
+	'interface-language': 'インターフェース言語',
+	'help': 'ヘルプ'
+  }
   });
   update_texts();
   
@@ -50,8 +65,11 @@ $(function() {
   e.preventDefault();
   $.i18n().locale = $(this).data('locale');
   $("#bear").attr("src","img/bear_"+$(this).data('locale')+".jpg");
+$( '#inputbox' ).prop( {
+    placeholder: $.i18n( 'name' ),
+    title: $.i18n( 'tooltip' )});
   update_texts();
 });
-  
+
   
 });
